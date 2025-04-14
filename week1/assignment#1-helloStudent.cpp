b968b3b1e14d056176546862e452d8662227d344
Luke <sachsluke1@gmail.com>
2025-04-14 16:14:41 -0700

    adding assignment 2 files

---
+++ week1/assignment#1-helloStudent.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    string password;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "! Welcome to C++!";
+    cout << "Please enter your password: ";
+    cin >> password;
+    cout << name << "! You should never share your password! (" << password << ")";
+    return 0;
+}
