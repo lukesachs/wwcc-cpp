523e444467304adaaf285de1985ab75c56cfc8e6
Luke <sachsluke1@gmail.com>
2025-04-14 17:44:31 -0700

    adding files

---
+++ week2/assignment#0-formatPractice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>   // For printf function
+using namespace std;
+
+int main() {
+    // Sample variables to display
+    int integer_value = 42;
+    float float_value = 3.14159265f;
+    double double_value = 3.14159265;
+    char char_value = 'X';
+    const char* string_value = "Hello, World!";
+
+    printf("Format Specifier Practice\n");
+    printf("------------------------\n\n");
+
+    // Your code here - use printf with different format specifiers
+
+    return 0;
+}
