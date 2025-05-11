dec28eaebc1a0e7b9f2ffa52e9070b6b6ac16d4c
Luke <sachsluke1@gmail.com>
2025-05-10 20:39:59 -0700

    fixed errors and changed some UI formatting

--- week5/midterm/userInterface.cpp
+++ week5/midterm/userInterface.cpp
@@ -12,7 +12,7 @@ void welcomeUser() {
 }
 
 void displayUserOptions(){
-    cout << "Please enter a number 1-4 to select:" << endl;
+    cout << "\nPlease enter a number 1-4 to select:" << endl;
     cout << "   1. Create bank account" << endl;
     cout << "   2. Access bank account info" << endl;
     cout << "   3. Make a transaction" << endl;
@@ -20,14 +20,14 @@ void displayUserOptions(){
 }
 
 void displayAccountOptions() {
-    cout << "Account options include 1. transaction history, 2. account statements, and 3. personal information." << endl;
+    cout << "\nAccount options include 1. transaction history, 2. account statements, and 3. personal information." << endl;
     cout << "   1. Access Transaction History" << endl;
     cout << "   2. Access Account Statement" << endl;
     cout << "   3. Access Personal Information" << endl;
 }
 
 void displayTransactionOptions() {
-    cout << "Please enter a number 1-3 to select:" << endl;
+    cout << "\nPlease enter a number 1-3 to select:" << endl;
     cout << "   1. Deposit Funds" << endl;
     cout << "   2. Withdraw Funds" << endl;
     cout << "   3. Transfer Funds" << endl;
@@ -38,12 +38,12 @@ void displayDate(Date d) {
 }
 
 void shutDown(){
-    cout << "Thank you for using the extremely legitimate banking service!\nWe hope you found our services to be helpful." << endl;
+    cout << "\nThank you for using the extremely legitimate banking service!\nWe hope you found our services to be helpful." << endl;
     cout << "\n\n\nSHUTTING DOWN......." << endl;
 }
 
 void getTransactionHistory(const Account &account){
-    cout << "Transaction History for " << account.holderName << ": ID#" << account.accountNumber << endl;
+    cout << "\nTransaction History for " << account.holderName << ": ID#" << account.accountNumber << endl;
     cout << "Total transaction count: " << account.transactionCount << endl;
     for(int i = 0; i < account.transactionCount; i++){
         cout << i + 1 << "." << endl; 
@@ -60,8 +60,8 @@ void getPersonalInfo(const Account &account){
 }
 
 void getAccountStatement(const Account &account){
-    cout << "*********** ACCOUNT STATEMENT ***********" << endl;
+    cout << "\n\n*********** ACCOUNT STATEMENT ***********" << endl;
     getPersonalInfo(account);
     getTransactionHistory(account);
-    cout << "*****************************************" << endl;
+    cout << "*****************************************\n" << endl;
 }
